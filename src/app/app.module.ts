5c87cf38a9b4121c7310abf612266ce2cbf8c90f
aniansson <ebba@aniansson.se>
2017-04-08 16:29:22 +0200

    Make some changes, add/delete files/code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
-import { WarningAlert } from './warning-alert/warning-alert.component';
+import { WarningAlertComponent } from './warning-alert/warning-alert.component';
 import { SuccessAlertComponent } from './success-alert/success-alert.component'
 
 @NgModule({
@@ -14,7 +14,7 @@ import { SuccessAlertComponent } from './success-alert/success-alert.component'
     AppComponent,
     ServerComponent,
     ServersComponent,
-    WarningAlert,
+    WarningAlertComponent,
     SuccessAlertComponent
   ],
   imports: [
--- src/app/warning-alert/warning-alert.component.ts
+++ src/app/warning-alert/warning-alert.component.ts
@@ -2,9 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-warning-alert',
-  templateUrl: 'warning-alert.component.html'
+  templateUrl: 'warning-alert.component.html',
+  styleUrls: ['./warning-alert.component.css']
 })
 
-export class WarningAlert {
+export class WarningAlertComponent {
 
 }
