c2a6c98ccf8e2ba872a5ba8016780b89e2f14c15
aniansson <ebba@aniansson.se>
2017-04-08 16:14:44 +0200

    Add element by class

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
+  /*Element itself/by class*/
+  /*selector: 'app-servers',*/
+
+  /*By attribute*/
   selector: '[app-servers]',
   template: '<app-server></app-server><app-server></app-server>',
   styleUrls: ['./servers.component.css']
