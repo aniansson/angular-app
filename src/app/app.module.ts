0a4190bf4570d7f10894c49e158805c0fc59b6f7
aniansson <ebba@aniansson.se>
2017-04-11 10:11:52 +0200

    Create new pages, add inupt field

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { Assignment2Component } from './assignment-2/assignment-2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Assignment2Component
   ],
   imports: [
     BrowserModule,
